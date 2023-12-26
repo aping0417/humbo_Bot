11ea9d4679090f74dfe413504a7fd87b1f128528
aping0417 <154878320+aping0417@users.noreply.github.com>
2023-12-26 19:07:35 +0800

    setting
    把我的token藏起來

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
+#--coding:utf-8--**
 import discord
 from discord.ext import commands
+import json
 
-bot=commands.Bot(intents=discord.Intents.default(),command_prefix= '+')
+with open('setting.json',mode='r',encoding='utf8')as jflie:
+    jdata = json.load(jflie)
+
+bot=commands.Bot(intents=discord.Intents.default(),command_prefix= '+') 
+
+
 
 @bot.event
 async def on_ready():
     print("憨包 啟動")
 
-bot.run('MTE4OTEzMTExMjA4NjI0NTQxNw.GhgJQg.6vfF0PlCWOi5xJOnOM1PooShHM_Ie7-Opmya1E')
+bot.run(jdata['TOKEN'])
