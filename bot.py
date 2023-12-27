2d153f3f7e3c5bfdf11af669ad9f9f55cfc4bc73
aping0417 <154878320+aping0417@users.noreply.github.com>
2023-12-27 22:27:38 +0800

    可以說話
    加了可以說話功能  但參數不會刪掉

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@
 import discord
 from discord.ext import commands
 import json
+import asyncio
 
 with open('setting.json',mode='r',encoding='utf8')as jflie:
     jdata = json.load(jflie)
 
-bot=commands.Bot(intents=discord.Intents.default(),command_prefix= '+') 
+bot=commands.Bot(intents=discord.Intents.all(),command_prefix= '+') 
 
 
 
@@ -14,4 +15,13 @@ bot=commands.Bot(intents=discord.Intents.default(),command_prefix= '+')
 async def on_ready():
     print("憨包 啟動")
 
-bot.run(jdata['TOKEN'])
+@bot.command()
+async def say(ctx:commands.Context):
+    await ctx.send(ctx.message.content)
+
+
+async def setup():
+    await bot.start(jdata["TOKEN"])
+
+asyncio.run(setup())
+#bot.run(jdata['TOKEN'])
