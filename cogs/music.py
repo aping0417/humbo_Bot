185214e887dbe0f33b783ccfaf3e69d08bf412c6
aping <pinghan0417@gmail.com>
2024-08-14 15:35:33 +0800

    能播音樂了
    格式問題解決

--- cogs/music.py
+++ cogs/music.py
@@ -57,20 +57,35 @@ class Music(Cog_Extension):
             # await interaction.response.send_message("語音客戶端不可用", silent=True)
             # return
             ydl_opts = {
-                "format": "audio/mpeg",
-                "quiet": True,
-                "extractaudio": True,
-                "outtmpl": "downloads/%(title)s.%(ext)s",
-                "noplaylist": True,
+                "format":"bestaudio/best",#格式
+                "quiet": True,#抑制 youtube_dl 的大部分输出
+                "extractaudio": True,#只抓聲音
+                "outtmpl": "downloads/%(title)s.%(ext)s",#指定下载文件的输出模板
+                "noplaylist": True,#禁用播放清單（之後會開放）
+                #'postprocessors': [{
+                    #'key': 'FFmpegExtractAudio',
+                    #'preferredcodec': 'm4a',  # 转换为 mp3
+                    #'preferredquality': '192',  # 设置比特率为192k
+                #}], （這些是限制版本）
             }
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=False)
-                url2 = info["formats"][0]["url"]
+                #for i, fmt in enumerate(info.get('formats', [])):
+                    #print(f"Format {i}: {fmt['format_id']} - {fmt['ext']} - {fmt['url']}")
+                    #(這是詳細的格式也是剛開始看的)
 
+                url2 = info["formats"][5]["url"] #第6個格式
+                
+                #downloaded_format = info.get('format')
+                #print(f"下载的格式: {downloaded_format}")
+                #(剛開始拿來看有啥格式能撥用的)
+            
+                
             ffmpeg_options = {
                 "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
                 "options": "-vn",
-            }
+            }#設定 -reconnect 1 （斷線自動重連） -reconnect_streamed 1（處理Streaming Media會自動重連）
+            #-reconnect_delay_max 5(斷線5秒內會自動重連) "options": "-vn" （只處理聲音）
             # voice_client.stop()
             voice_client.play(
                 discord.FFmpegPCMAudio(url2, **ffmpeg_options),
