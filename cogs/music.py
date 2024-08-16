9d37e01b042c884224fe67568d077c2af4cdcc01
smallsnow <vm5230230@gmail.com>
2024-08-16 17:19:45 +0800

    傳網址加靜音,整體加排版

--- cogs/music.py
+++ cogs/music.py
@@ -47,56 +47,56 @@ class Music(Cog_Extension):
         try:
             if interaction.user.voice is None:
                 await interaction.response.send_message(
-                "你沒有加入任何語音頻道", ephemeral=True
-            )
-                return      
+                    "你沒有加入任何語音頻道", ephemeral=True
+                )
+                return
             voice＿channel = interaction.user.voice.channel
             voice＿client = interaction.guild.voice_client
-           
-            if voice_client is  None: 
+
+            if voice_client is None:
                 voice＿client = await voice＿channel.connect()
-                #await interaction.response.send_message(f"網址{url}")
+                # await interaction.response.send_message(f"網址{url}")
             elif voice＿client.channel != voice_channel:
-                await voice＿channel._move(position=voice_client,reason=None)
-            #else:
-                #return
-                #downloaded_format = info.get('format')
-                #print(f"下载的格式: {downloaded_format}")
-                
+                await voice＿channel._move(position=voice_client, reason=None)
+            # else:
+            # return
+            # downloaded_format = info.get('format')
+            # print(f"下载的格式: {downloaded_format}")
+
             # if not isinstance(voice_client, discord.VoiceClient):
             # await interaction.response.send_message("語音客戶端不可用", silent=True)
             # return
             ydl_opts = {
-                "format":"bestaudio/best",#格式
-                "quiet": True,#抑制 youtube_dl 的大部分输出
-                "extractaudio": True,#只抓聲音
-                "outtmpl": "downloads/%(title)s.%(ext)s",#指定下载文件的输出模板
-                "noplaylist": True,#禁用播放清單（之後會開放）
+                "format": "bestaudio/best",  # 格式
+                "quiet": True,  # 抑制 youtube_dl 的大部分输出
+                "extractaudio": True,  # 只抓聲音
+                "outtmpl": "downloads/%(title)s.%(ext)s",  # 指定下载文件的输出模板
+                "noplaylist": True,  # 禁用播放清單（之後會開放）
                 #'postprocessors': [{
-                    #'key': 'FFmpegExtractAudio',
-                    #'preferredcodec': 'm4a',  # 转换为 mp3
-                    #'preferredquality': '192',  # 设置比特率为192k
-                #}], （這些是限制版本）
+                #'key': 'FFmpegExtractAudio',
+                #'preferredcodec': 'm4a',  # 转换为 mp3
+                #'preferredquality': '192',  # 设置比特率为192k
+                # }], （這些是限制版本）
             }
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=False)
-                songtitle = info.get('title', None)
-                #for i, fmt in enumerate(info.get('formats', [])):
-                    #print(f"Format {i}: {fmt['format_id']} - {fmt['ext']} - {fmt['url']}")
-                    #(這是詳細的格式也是剛開始看的)
+                songtitle = info.get("title", None)
+                # for i, fmt in enumerate(info.get('formats', [])):
+                # print(f"Format {i}: {fmt['format_id']} - {fmt['ext']} - {fmt['url']}")
+                # (這是詳細的格式也是剛開始看的)
 
-                url2 = info["formats"][5]["url"] #第6個格式
-                
-                #downloaded_format = info.get('format')
-                #print(f"下载的格式: {downloaded_format}")
-                #(剛開始拿來看有啥格式能撥用的)
-            
-                
+                url2 = info["formats"][5]["url"]  # 第6個格式
+
+                # downloaded_format = info.get('format')
+                # print(f"下载的格式: {downloaded_format}")
+                # (剛開始拿來看有啥格式能撥用的)
+
             ffmpeg_options = {
                 "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
                 "options": "-vn",
-            }#設定 -reconnect 1 （斷線自動重連） -reconnect_streamed 1（處理Streaming Media會自動重連）
-            #-reconnect_delay_max 5(斷線5秒內會自動重連) "options": "-vn" （只處理聲音）
+            }  # 設定 -reconnect 1 （斷線自動重連） -reconnect_streamed 1（處理Streaming Media會自動重連）
+
+            # -reconnect_delay_max 5(斷線5秒內會自動重連) "options": "-vn" （只處理聲音）
             # voice_client.stop()
             async def playmusic():
                 try:
@@ -105,15 +105,23 @@ class Music(Cog_Extension):
                         after=lambda e: print(f"Player error: {e}") if e else None,
                     )
                 except Exception as e:
-                    await interaction.response.send_message(f"音樂錯誤：{str(e)}",silent=True)
+                    await interaction.response.send_message(
+                        f"音樂錯誤：{str(e)}", silent=True
+                    )
+
             async def sendmsg():
                 try:
                     print("我自你前面")
-                    await interaction.response.send_message(f"歌名{songtitle}\n網址{url}")
+                    await interaction.response.send_message(
+                        f"歌名{songtitle}\n網址{url}", silent=True
+                    )
                     print("我自你後面")
                 except Exception as e:
-                    await interaction.response.send_message(f"訊息錯誤：{str(e)}",silent=True)
-            await asyncio.gather(playmusic(),sendmsg())
+                    await interaction.response.send_message(
+                        f"訊息錯誤：{str(e)}", silent=True
+                    )
+
+            await asyncio.gather(playmusic(), sendmsg())
             # return
         except Exception as e:
             await interaction.response.send_message(f"發生錯誤：{str(e)}", silent=True)
